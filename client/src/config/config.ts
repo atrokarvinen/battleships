c70d6b05535755ff64aed8fc7488271d3cf5f204
Atro Karvinen <atrok@hotmail.com>
2023-07-25 17:53:44 +0300

    log url

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,3 +1,5 @@
+console.log("baseUrl: ", import.meta.env.VITE_BACKEND_BASE_URL);
+
 export const config = {
   // backendBaseUrl: "https://battleships-app.fly.dev",
   backendBaseUrl: import.meta.env.VITE_BACKEND_BASE_URL,
